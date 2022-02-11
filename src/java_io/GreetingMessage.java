c92d3812de4f14cefd3b13db2a0c1f927c946beb
Mohammed Rehan <rsayed@intelinotion.com>
2022-02-11 06:30:40 +0530

    Updated file path and Added a new class to greet user

---
+++ src/java_io/GreetingMessage.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GreetingMessage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String nameOfPerson = input.nextLine();
+        System.out.println("Welcome " + nameOfPerson + " to the world of programing");
+    }
+}
