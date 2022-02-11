a96b15542999747e26cf1a106801253c44115a6b
Mohammed Rehan <rsayed@intelinotion.com>
2022-02-11 09:45:18 +0530

    Updated code for calculation of fibonacci series

--- src/java_io/CalculateFibonacciSeries.java
+++ src/java_io/CalculateFibonacciSeries.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CalculateFibonacciSeries {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
         int nthNumber = scanner.nextInt();
         int previousValue = 0;
         int currentValue = 1;
