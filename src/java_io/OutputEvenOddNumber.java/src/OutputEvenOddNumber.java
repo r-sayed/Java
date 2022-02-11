10ec6932591573941cf05513b1b48e68ec78b21c
Mohammed Rehan <rsayed@intelinotion.com>
2022-02-11 05:57:14 +0530

    Added code to identify even and odd number in java

---
+++ src/java_io/OutputEvenOddNumber.java/src/OutputEvenOddNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class OutputEvenOddNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int inputNumber = (int) input.nextDouble();
+        if (inputNumber % 2 == 0) {
+            System.out.print("Entered number is an even number");
+        } else {
+            System.out.print("Entered number is an odd number");
+        }
+    }
+}
